4d92c89c2257fdb70bcd8b7b6d98279625b14ad5
DineshReddy420 <64855777+DineshReddy420@users.noreply.github.com>
2020-11-16 19:35:56 -0500

    New Movielist actvity

---
+++ app/src/main/java/com/example/custome_listview/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.custome_listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.Image;
+import android.os.Bundle;
+import android.widget.ListView;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
